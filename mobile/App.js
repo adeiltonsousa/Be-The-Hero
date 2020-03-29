ff7f80b8aaec600332152864afaa08c4ecf51468
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2020-03-29 11:33:05 -0300

    Pagina de ONGs e Routes configuradas

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
+
+import Incidents from './src/pages/Incidents'
+
+export default function App() {
+  return (
+   <Routes />
+  );
+}
